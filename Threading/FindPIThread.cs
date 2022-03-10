41b05b3e9c7c21af2f99304547e27a36e3513ff9
Lucky Woods <lucjwood@ut.utm.edu>
2022-03-10 12:16:56 -0600

    Add project files.

---
+++ Threading/FindPIThread.cs
@@ -0,0 +1,58 @@
+﻿/*
+* Author: Lucky Jaxxon Woods
+* Date: 3/09/22
+* File: FindPIThread.cs
+* Description: A C# file containing the implementation of the FindPIThread file.
+*
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threading
+{
+    class FindPIThread
+    {
+
+        int dartNum;
+        int dartHit;
+        Random numb;
+
+        public FindPIThread(int dart)
+        {
+            numb = new Random();
+            dartNum = dart;
+            dartHit = 0;
+        }
+
+        public void throwDarts()
+        {
+            //FindPIThread findPIThread(1);
+
+            for (int i = 0; i < dartNum; i++)
+            {
+                numb = new Random();
+                double x = numb.NextDouble();
+                numb = new Random();
+                double y = numb.NextDouble();
+
+                double z = Math.Sqrt((y*y) + (x*x));
+
+                if (1.0 > z)
+                {
+                    dartHit++;
+                }
+            }
+        }
+
+        public void printHits()
+        {
+            Console.WriteLine(dartHit);
+
+        }
+
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+* Author: Lucky Jaxxon Woods
+* Date: 3/09/22
+* File: program.cs
+* Description: A C# file containing the implementation of the main program.
+*
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many darts?");
+            string intConvert = Console.ReadLine();
+            int dartCount = int.Parse(intConvert);
+
+            Console.WriteLine("How many threads");
+            string threadConvert = Console.ReadLine();
+            int threadCount = int.Parse(threadConvert);
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                FindPIThread loop = new FindPIThread(dartCount);
+                Thread myThread = new Thread(new ThreadStart(loop.throwDarts));
+                myThread.Start();
+                Thread.Sleep(16);
+                loop.printHits();
+            }
+        }
+    }
+}
